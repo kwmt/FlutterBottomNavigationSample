bd0197a27f32bdb2a96d6c76bfcbe0b0e3e9b8a6
Yasutaka Kawamoto <kwmt27@gmail.com>
2018-08-17 16:18:18 +0900

    タブタップで画面を切り替えるサンプル追加

--- lib/home_widget.dart
+++ lib/home_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bottom_navigation/placeholder_widget.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,19 +7,37 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    PlaceHolderWidget(Colors.white),
+    PlaceHolderWidget(Colors.deepOrange),
+    PlaceHolderWidget(Colors.green),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('My Flutter App'),
       ),
-      bottomNavigationBar: BottomNavigationBar(currentIndex: 0, items: [
-        BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.mail), title: Text('Messages')),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.person), title: Text('Profile'))
-      ]),
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: onTabTapped,
+          currentIndex: _currentIndex,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home), title: Text('Home')),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.mail), title: Text('Messages')),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person), title: Text('Profile'))
+          ]),
     );
+  }
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
   }
 }
